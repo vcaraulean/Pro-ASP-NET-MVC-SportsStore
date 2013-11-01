dc9c75d7bca25c23f646ec590ca26b7b2a8f7fe1
Valeriu Caraulean <valeriu.caraulean@b-i.com>
2013-11-01 16:52:22 +0100

    Chapter 8, finished

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            var line = lineCollection.FirstOrDefault(i => i.Product.ProductID == product.ProductID);
+
+            if (line == null)
+                lineCollection.Add(new CartLine { Product = product, Quantity = quantity });
+            else
+                line.Quantity++;
+        }
+
+        public void RemoveLine(Product product)
+        {
+            lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+        }
+
+        public decimal TotalValue()
+        {
+            return lineCollection.Sum(line => line.Product.Price * line.Quantity);
+        }
+
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+}
